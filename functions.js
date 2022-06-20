78d829c4c974e25a3cea33f5726d578d8f4265f3
Duncan Temple Lang <duncan@r-project.org>
2022-06-20 13:46:50 -0700

    trying to add a  content_scripts extension component to catch all hyperlink events

---
+++ functions.js
@@ -0,0 +1,45 @@
+function jumpToTabByURL(url, exact)
+{
+//    alert("jumpToTabByURL " + url + " exact = " + exact );
+    console.log("looking for " + url + " " + exact);
+    var found = false;
+    var pr = browser.windows.getAll({ populate: true });
+    pr.then(
+	function(windowList) {
+	    windowList.forEach(function(window) {
+		window.tabs.forEach(function(tab) {
+		    console.log(tab + " " +  tab.url);
+		    var ok = exact ? tab.url == url : tab.url.match(url) !== null;
+		    if(!ok) 
+			ok = exact ? tab.title == url : tab.title.match(url) !== null;
+		    
+		    if(ok) {
+			// console.log("found tab for " + url + " " + tab);
+			showTab2(tab, window);
+			found = true;
+			console.log("found tab " + tab.url);
+			// Can stop the looping here but how - throw an error, return false/true ?
+		    }
+		    
+		})
+	    })
+	    if(!found) 
+		browser.tabs.create({ url: url}) ;
+
+	},
+        err => console.log("error: "+ err));
+}
+
+
+function showTab2(tab, window)
+{
+    browser.tabs.update(tab.id, {active: true});
+
+    console.log("showTab2: " + tab.windowId);
+
+	  // if the tab is not in the current window, bring that window to the front.
+    if(window && !window.active)
+	chrome.windows.update(window.id, {focused: true});
+    else
+	chrome.windows.update(tab.windowId, {focused: true});
+}
